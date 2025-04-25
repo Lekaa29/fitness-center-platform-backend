cc33475e999214f5221a4b2b144c38e8be054c7b
Lekaa29 <orvol905@gmail.com>
2025-04-25 19:57:42 +0200

    setup sqlite, datacontext, import gimmick models
    # Conflicts:
    #	FitnessCenterAPI/EctorisNekretnine.csproj
    #	obj/Debug/net9.0/FitnessCenterApi.AssemblyInfo.cs
    #	obj/Debug/net9.0/FitnessCenterApi.AssemblyInfoInputs.cache
    #	obj/Debug/net9.0/FitnessCenterApi.assets.cache
    #	obj/Debug/net9.0/FitnessCenterApi.csproj.AssemblyReference.cache
    #	obj/FitnessCenterApi.csproj.nuget.dgspec.json
    #	obj/FitnessCenterApi.csproj.nuget.g.props
    #	obj/FitnessCenterApi.csproj.nuget.g.targets
    #	obj/project.assets.json
    #	obj/project.nuget.cache
    #	obj/project.packagespec.json
    #	obj/rider.project.model.nuget.info
    #	obj/rider.project.restore.info

---
+++ Models/FitnessCentar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessCenterApi.Models;
+
+public partial class FitnessCentar
+{
+    public int IdFitnessCentar { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Membership> Memberships { get; set; } = new List<Membership>();
+}
